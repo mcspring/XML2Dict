69be71d44d1970bf69eca30f1a8fbd212bc4491c
MC <mcspring@Spring.local>
2011-03-23 16:50:32 +0800

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- conding=utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='XMl2Dict',
+    version='0.1',
+    author='Spring Mc',
+    author_email='Heresy.Mc@gmail.com',
+    packages=['encoder', 'decoder'],
+    url='https://github.com/mcspring/XML2Dict',
+    license='http://www.apache.org/licenses/LICENSE-2.0.html',
+    description='Convert between XML String and Python Dict',
+    long_description=open('README.md').read())
